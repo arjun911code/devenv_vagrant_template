6ef200ec86685b084b0cc263225238726a438b1d
Arjunkumar <arjun911code@gmail.com>
2015-11-09 10:34:51 -0800

    Created new vagrant+puppet modules for cassandra stat collection
    Modules for Installing:
    1. "python", "python-dev", "python-pip"
    2. Modified requirements.txt

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,12 +1,2 @@
 Exec { path => ["/bin/", "/usr/bin", "/usr/local/bin/"] }
 import 'nodes.pp'
-/*
-user { 'arjun':
-    ensure => 'present',
-    groups => ['sudo'],
-    home => '/home/arjun',
-    managehome => true,
-    password => "test",
-    shell => '/bin/bash',
-    }
-*/
--- puppet/modules/requirements/manifests/init.pp
+++ puppet/modules/requirements/manifests/init.pp
@@ -1,24 +1,23 @@
-class django {
-    include django::install
+class requirements {
+
 }
 
-class django::install {
+class requirements::install {
     exec { "update-apt":
-        command => "sudo apt-get update",
+        command => "sudo apt-get update"
     }
 
-    package { ["python", "python-dev", "python-pip", "python-pandas"]:
+    package{ ["python", "python-dev", "python-pip"]:
         ensure => present,
         require => Exec['update-apt'],
     }
 
-    exec { "pip install":
+    exec {"pip install":
         command => "sudo /usr/bin/pip install -r /vagrant/requirements.txt",
-        tries => 3,
+        tries =>3,
         timeout => 600,
         require => Package["python", "python-pip"],
         logoutput => on_failure,
     }
+
 }
-
-
